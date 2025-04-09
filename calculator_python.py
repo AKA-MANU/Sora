ad013d6af82489fafb5bc384af3d62cf307c645f
Sora <152990278+AKA-MANU@users.noreply.github.com>
2025-04-09 23:38:41 +0545

    Update calculator_python.py
    Add discount option!

--- calculator_python.py
+++ calculator_python.py
@@ -3,16 +3,17 @@ while True:
     num_1 = float(input("Enter the first number: "))
     num_2 = float(input("Enter the second number: "))
 
-    print("Enter the operation: ")
+    print("\nEnter the operation: ")
     print("1.ADD")
     print("2.SUB")
     print("3.MULTIPLY")
     print("4.DIVIDE")
-    print("5.EXIT")
-    choose = input("Enter the choice (1-5): ")
+    print("5.Discount")
+    print("6.EXIT")
+    choose = input("Enter the choice (1-6): ")
 
 
-    if choose =="5":
+    if choose =="6":
         print("Exitting,Good_Bye")
         break
 
@@ -28,7 +29,23 @@ while True:
                    
         else:
             print(f"{num_1} / {num_2} = {num_1 / num_2:.2f}")
-    
+            
+    elif choose == "5":
+        def calculate_discount (total,discount_percent):
+            discount_amount = total * (discount_percent / 100)
+            discounted_total = total - discount_amount
+            return discounted_total
+            
+        total_cost = float(input("Enter the total_cost: "))
+        discount = input("Do you have discount coupon?:,yes/no")
+        
+        if discount == "yes":
+            discount_percent = float(input("Enter the discount_percent: "))
+            final_cost = calculate_discount(total_cost,discount_percent)
+            print("Your final cost after discount:", final_cost)
+        else:
+            ("Your final cost is :",final_cost)        
+
     else: 
         print("Please choose the right option")          
 
